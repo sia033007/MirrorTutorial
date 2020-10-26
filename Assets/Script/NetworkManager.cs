efcac6157748996abddf3995a6fc63c2482f2abb
DESKTOP-8IO3ALF\HeisenBerg <siavashmehmandoost@gmail.com>
2020-10-26 00:56:06 -0700

    Tutorial1

---
+++ Assets/Script/NetworkManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class NetworkManager : NetworkBehaviour
+{
+    private void Update() {
+        if(isLocalPlayer){
+            CmdMove();
+           
+        }
+    }
+    [Command]
+    void CmdMove(){
+        RpcMove();
+    }
+    [ClientRpc]
+    void RpcMove(){
+            float horizpntalmove = Input.GetAxis ("Horizontal");
+            float verticalmove = Input.GetAxis ("Vertical");
+            Vector3 movement= new Vector3 (horizpntalmove *0.1f,verticalmove*0.1f,0);
+            transform.position = transform.position + movement;
+    }
+}
