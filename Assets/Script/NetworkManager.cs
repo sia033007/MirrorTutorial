b238b706a827fcba381f68b94b74dc2ba0e7427b
DESKTOP-8IO3ALF\HeisenBerg <siavashmehmandoost@gmail.com>
2020-10-26 01:28:16 -0700

    Tutorial2

--- Assets/Script/NetworkManager.cs
+++ Assets/Script/NetworkManager.cs
@@ -5,21 +5,13 @@ using Mirror;
 
 public class NetworkManager : NetworkBehaviour
 {
+    [Client]
     private void Update() {
         if(isLocalPlayer){
-            CmdMove();
-           
-        }
-    }
-    [Command]
-    void CmdMove(){
-        RpcMove();
-    }
-    [ClientRpc]
-    void RpcMove(){
             float horizpntalmove = Input.GetAxis ("Horizontal");
             float verticalmove = Input.GetAxis ("Vertical");
             Vector3 movement= new Vector3 (horizpntalmove *0.1f,verticalmove*0.1f,0);
             transform.position = transform.position + movement;
+        }       
     }
 }
