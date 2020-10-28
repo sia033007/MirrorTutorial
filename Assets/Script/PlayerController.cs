59ee3c8624373477610f09b04a96d1b88271c05b
DESKTOP-8IO3ALF\HeisenBerg <siavashmehmandoost@gmail.com>
2020-10-28 10:53:33 -0700

    Tutorial6

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerController : NetworkBehaviour
+{
+    Rigidbody rb;
+    public GameObject spawnPos;
+    public GameObject bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isLocalPlayer){
+             CmdMove();
+             if(Input.GetKeyDown(KeyCode.Space)){
+                 CmdSpawn();
+             }
+        }
+       
+    }
+    [Command]
+    void CmdMove(){
+        RpcMove();
+    }
+    [ClientRpc]
+    void RpcMove(){
+        if(!isServer){
+            MovePlayer();
+        }
+       
+    }
+    [Command]
+    void CmdSpawn(){
+        RpcSpawn();
+    }
+    [ClientRpc]
+    void RpcSpawn(){
+        GameObject bullet = Instantiate (bulletPrefab,spawnPos.transform.position,Quaternion.identity);
+        bullet.transform.SetParent(spawnPos.transform);
+        bullet.GetComponent<Rigidbody>().AddForce(Vector3.forward *1000f);
+        Destroy(bullet.gameObject,2f);
+
+    }
+    void MovePlayer(){
+        float horizontalmove = Input.GetAxis("Horizontal");
+        float verticalmove = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(horizontalmove *5f,verticalmove *5f,0);
+
+    }
+}
